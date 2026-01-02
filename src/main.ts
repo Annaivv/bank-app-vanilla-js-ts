839e95e21bcde93fe2d2365c88da4acb31ab0a3b
annaivv <annashalya@gmail.com>
2026-01-02 20:58:57 +0100

    feature: transfer money functionality added

--- src/main.ts
+++ src/main.ts
@@ -50,3 +50,39 @@ buttonsUI.login.addEventListener("click", function (e) {
     updateUI(currentAccount);
   }
 });
+
+// Transfer money to other user
+buttonsUI.transfer.addEventListener("click", function (e) {
+  e.preventDefault();
+  const amount = +inputsUI.transferAmount.value;
+  const receiverAcc = accounts.find(
+    (acc) => acc.username === inputsUI.transferTo.value
+  );
+  inputsUI.transferAmount.value = inputsUI.transferTo.value = "";
+
+  if (!currentAccount) console.log("No current account");
+
+  if (
+    currentAccount &&
+    amount > 0 &&
+    receiverAcc &&
+    (currentAccount.balance ?? 0) >= amount &&
+    receiverAcc.username !== currentAccount?.username
+  ) {
+    // Doing the transfer
+    currentAccount.movements.push(-amount);
+    console.log(currentAccount.movements);
+    receiverAcc.movements.push(amount);
+
+    // Add transfer date
+    currentAccount.movementsDates.push(new Date().toISOString());
+    receiverAcc.movementsDates.push(new Date().toISOString());
+
+    // Update UI
+    updateUI(currentAccount);
+
+    // Reset timer
+    // clearInterval(timer);
+    // timer = startLogOutTimer();
+  }
+});
